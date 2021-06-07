721df189e98e296dfae7727d25f38216c0995b03
Mark4551124015 <Mark4551124015@gmail.com>
2021-06-08 01:15:09 +0800

    Update 打包.bat

--- javsdt/打包.bat
+++ javsdt/打包.bat
@@ -1,5 +1,12 @@
 @echo off
 chcp 65001
+mode con cols=81 lines=20
+TITLE Javsdt打包工具
+
+:MENU
+CLS
+echo. 
+echo.                                      打包工具
 ECHO. ===============================================================================
 echo. =                                                                             =
 echo. =         [1]打包 素人Jav321  [2]打包 创建ini    [3] 打包 javdbFc2            =
@@ -8,12 +15,13 @@ echo. =         [4]打包 Javbus无码  [5]打包 Javbus有码 [6] 打包 Javlib
 echo. =                                                                             =
 ECHO. ===============================================================================
 echo.
+echo. 
 :choice
 set choice=
 set /p choice= 请输入选项:
 if /i "%choice%"=="1" goto jav321
-if /i "%choice%"=="2" goto Fc2
-if /i "%choice%"=="3" goto Createini
+if /i "%choice%"=="2" goto Createini
+if /i "%choice%"=="3" goto Fc2
 if /i "%choice%"=="4" goto BusW
 if /i "%choice%"=="5" goto BusY
 if /i "%choice%"=="6" goto lib
@@ -25,23 +33,23 @@ goto choice
 
 :jav321
 echo. 开始打包
-pyinstller -F Jav321.py 
+pyinstaller -F Jav321.py 
 cd %~dp0\dist
-move /y Jav321.exe 【素人】jav321须翻墙.exe
+move /y "Jav321.exe" "【素人】jav321 须翻墙.exe"
 goto end
 
 :Fc2
 echo. 开始打包
 pyinstaller -F JavdbFc2.py
 cd dist
-move /y JavdbFc2.exe 【FC2】javdb.exe
+move /y "JavdbFc2.exe" 【FC2】javdb.exe
 goto end
 
 :Createini
 echo. 开始打包
-pyinstaller -F CrateIni.py
+pyinstaller -F CreateIni.py
 cd dist
-move /y CrateIni.exe 重新创建ini.exe
+move /y CreateIni.exe 重新创建ini.exe
 goto end
 
 :BusW
@@ -66,5 +74,7 @@ move /y javlibrary.exe 【有码】javlibrary.exe
 goto end
 
 :end
-echo. 结束
+echo. 回车继续打包
 pause 1>nul 2>nul
+cd ..
+goto MENU
